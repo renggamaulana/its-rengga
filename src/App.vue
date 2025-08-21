05c87585710aa174125317870bcd3f5e40b76da3
renggamaulana <renggaa21@gmail.com>
2025-08-21 08:10:38 +0700

    first init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex justify-between p-20 gap-10">
+    <div class="flex-1">
+      <figure>
+        <h1 class="text-5xl font-semibold">Rengga Maulana</h1>
+        <h2>Full Stack Engineer</h2>
+        <p class="w-3/4">I create end-to-end digital experiences that are intuitive, accessible, and built to last.</p>
+      </figure>
+    </div>
+    <div class="flex-1">
+      <main>
+        <h1>About</h1>
+      </main>
+    </div>
+  </div>
+</template>
