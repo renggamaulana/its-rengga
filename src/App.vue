ce6ef08a21af0da7bebcaa764d48603521c93165
Rengga <renggaa21@gmail.com>
2025-08-21 12:10:26 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,21 @@
 </script>
 
 <template>
-  <div class="flex justify-between p-20 gap-10">
+  <div class="flex justify-between px-96 py-24 gap-10">
     <div class="flex-1">
       <figure>
-        <h1 class="text-5xl font-semibold">Rengga Maulana</h1>
-        <h2>Full Stack Engineer</h2>
-        <p class="w-3/4">I create end-to-end digital experiences that are intuitive, accessible, and built to last.</p>
+        <h1 class="text-5xl font-semibold ">Rengga Maulana</h1>
+        <h2 class="my-4 font-semibold text-lg">Full Stack Engineer</h2>
+        <p class="w-3/4 font-light">I create end-to-end digital experiences that are intuitive, accessible, and built to last.</p>
       </figure>
     </div>
     <div class="flex-1">
+      <!-- about -->
       <main>
-        <h1>About</h1>
+        <p>I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that blend thoughtful design with robust engineering. My favorite work lies at the intersection of design and development, creating experiences that not only look great but are meticulously built for performance and usability.</p>
+        <p>Currently, I'm a Senior Front-End Engineer at Klaviyo, specializing in accessibility. I contribute to the creation and maintenance of UI components that power Klaviyo’s frontend, ensuring our platform meets web accessibility standards and best practices to deliver an inclusive user experience.</p>
+        <p>In the past, I've had the opportunity to develop software across a variety of settings — from advertising agencies and large corporations to start-ups and small digital product studios. Additionally, I also released a comprehensive video course a few years ago, guiding learners through building a web app with the Spotify API.</p>
+        <p>In my spare time, I’m usually climbing, reading, hanging out with my wife and two cats, or running around Hyrule searching for Korok seeds.</p>
       </main>
     </div>
   </div>
